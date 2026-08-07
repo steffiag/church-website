a649ace9a778440d575dc5449e8090cbbfa864a0
steffiag <steffiageorge@gmail.com>
2026-08-07 13:37:20 -0400

    change

--- church-app/src/components/Navbar.jsx
+++ church-app/src/components/Navbar.jsx
@@ -130,7 +130,7 @@ function Navbar() {
             </div>
           </li>
 
-          <li onMouseEnter={clearDropdownFocus}><Link to="/programs">Programs</Link></li>
+          <li className="nav-item-dropdown" onMouseEnter={clearDropdownFocus}><Link to="/programs">Programs</Link></li>
 
           <li className="nav-item-dropdown" onMouseEnter={clearDropdownFocus}>
             <button
