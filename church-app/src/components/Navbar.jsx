8a0749170c3ef0aaa8bc1ab1e5f22d355fbd5d0d
steffiag <steffiageorge@gmail.com>
2026-08-21 20:18:08 -0400

    change

--- church-app/src/components/Navbar.jsx
+++ church-app/src/components/Navbar.jsx
@@ -126,6 +126,7 @@ function Navbar() {
               <Link to="/about#mission">Mission &amp; Vision</Link>
               <Link to="/about#history">History</Link>
               <Link to="/about#patriarch">Patriarch</Link>
+              <Link to="/about#catholicos">Catholicos</Link>
               <Link to="/about#archbishop">Archbishop</Link>
               <Link to="/about#vicar">Vicar</Link>
               <Link to="/about#leadership">Leadership</Link>
